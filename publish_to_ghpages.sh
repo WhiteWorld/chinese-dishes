259757f254f76147e0e8a166820800a8f8ca79bf
Liu <ljq258@gmail.com>
2018-02-25 15:37:33 +0800

    a

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -25,5 +25,6 @@ echo "Generating site"
 hugo
 
 echo "Updating gh-pages branch"
+echo 'www.chinesedishes.net' > public/CNAME
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)" && cd ..
 git push origin gh-pages
